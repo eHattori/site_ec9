e1cb1f1c559d100384717b9c095e8bffba92c485
Eduardo Hattori <eduardo.hattori@ec9.com.br>
2012-08-11 05:33:09 -0300

    estrutura basica

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteEc9::Application.config.secret_token = 'a7d4c2b36917c62dbc609cb0b6104b6e41fdb09584769d9f537445c58427a672885d1b8dc4292c79ecba6489287b49d4226471afc7e24e6e6371c842f878de73'
